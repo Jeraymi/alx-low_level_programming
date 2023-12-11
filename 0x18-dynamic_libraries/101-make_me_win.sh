26920b5675b159df7e61e70ef283190f50e7464a
Jeraymi <jeremiahanazodo@gmail.com>
2023-12-11 15:25:56 -0800

    if this doesn't work again, i might run mad

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
+wget -P /tmp https://github.com/faithful1ofall/alx-low_level_programming/raw/master/0x18-dynamic_libraries/Fwin.so
+export LD_PRELOAD=/tmp/Fwin.so
